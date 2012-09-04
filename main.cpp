34e67f9961d7f6372bb32bfd7836d0f6c5657844
Niklas Wigertz Danielsson <rymdo.sl@gmail.com>
2012-09-04 08:36:48 +0200

    Added lib/SDL.dll

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 /* 
- * File:   main.cpp
- * Author: Rymdo
+ * File:        main.cpp
+ * Authors:     Niklas Wigertz Danielsson
+ *              Marcus Beidegårdh
+ *              Emelie Lannerhjelm
+ *              Daniel Käll
  *
  * Created on den 4 september 2012, 08:21
  */
-
 #include <cstdlib>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
     return 0;
