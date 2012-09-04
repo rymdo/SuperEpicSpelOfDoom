fd41c059034177336dfa74a182d71126c5029ca8
Niklas Wigertz Danielsson <rymdo.sl@gmail.com>
2012-09-04 08:22:00 +0200

    Initial setup
    Setup of project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Rymdo
+ *
+ * Created on den 4 september 2012, 08:21
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
